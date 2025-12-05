f03d3d00d4fe6e8c961af94f85cfc25554c7722d
Tester <dragaomatadordeporco@gmail.com>
2025-12-05 16:31:32 -0300

    Add files via upload

---
+++ include/philo.h
@@ -0,0 +1,76 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+# define PHILO_MAX 200
+# define MIN_TIME 60
+
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_rules
+{
+	unsigned long time_to_die;
+	unsigned long time_to_eat;
+	unsigned long time_to_sleep;
+	unsigned long start_time;
+	int philo;
+	int max_eats;
+	int	someone_died;
+	pthread_mutex_t *forks;
+	pthread_mutex_t print_mutex;
+	pthread_mutex_t meal_mutex;
+} t_rules;
+
+typedef struct s_philo
+{
+	int id_philo;
+	int meals_eaten;
+	unsigned long last_meal_time;
+	t_rules *rules;
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t *right_fork;
+	pthread_t thread;
+} t_philo;
+
+//-----------------init-------------------
+
+t_rules	*init_rules(int argc, char **argv);
+t_philo *init_philo(t_rules *rules);
+int		start_threads(t_rules *rules, t_philo *philos);
+
+//-----------------utils------------------
+
+void	*ft_calloc(size_t nmemb, size_t size);
+int		ft_atoi(const char *nprt);
+int		ft_isdigit(int c);
+void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(const char *str);
+
+//----------------free--------------------
+
+t_rules *free_rules(t_rules *rules);
+
+//---------------mutex--------------------
+
+void 	destroy_all_mutexes(int i, t_rules *rules);
+int 	init_all_mutexes(int i, t_rules *rules);
+int 	thread_failure(t_philo *philos, int threads_created);
+
+//---------------monitor------------------
+
+unsigned long get_time_ms(void);
+void 		  precise_usleep(unsigned long time_in_ms);
+void	      *monitor_routine(void *arg);
+
+//---------------actions-------------------
+
+void 	philo_take_forks(t_philo *philo);
+void	print_action(t_philo *philo, char *msg);
+void	philo_action(t_philo *philo, int actions);
+void 	philo_forks_or_eating_or_sleep(t_philo *philo, int actions);
+void 	*philo_routine(void *arg);
+
+#endif
---
+++ src/routine.c
@@ -0,0 +1,42 @@
+#include "philo.h"
+
+static void	handle_one_philo(t_philo *philo)
+{
+    philo_action(philo, 4);
+    precise_usleep(philo->rules->time_to_die);
+    pthread_mutex_lock(&philo->rules->print_mutex);
+    printf("%lu %d died\n", get_time_ms() - philo->rules->start_time,
+        philo->id_philo);
+    pthread_mutex_unlock(&philo->rules->print_mutex);
+    philo->rules->someone_died = 1;
+}
+
+static void	philo_loop(t_philo *philo)
+{
+    while (philo->rules->someone_died == 0 &&
+        (philo->rules->max_eats == 0 ||
+        philo->meals_eaten < philo->rules->max_eats))
+    {
+        philo_action(philo, 1);
+        philo_forks_or_eating_or_sleep(philo, 1);
+        if (philo->rules->someone_died)
+            break ;
+        philo_forks_or_eating_or_sleep(philo, 2);
+        if (philo->rules->someone_died)
+            break ;
+        philo_forks_or_eating_or_sleep(philo, 3);
+    }
+}
+
+void	*philo_routine(void *arg)
+{
+    t_philo *philo;
+
+    philo = (t_philo *)arg;
+    if (philo->rules->philo == 1)
+        return (handle_one_philo(philo), NULL);
+    if (philo->id_philo % 2 == 0)
+        usleep(100);
+    philo_loop(philo);
+    return (NULL);
+}
